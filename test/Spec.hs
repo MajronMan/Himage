7cfbcaa483882dfdc62dc2b4ea31d58457ab78e6
Majron Man <majron15@gmail.com>
2017-01-16 10:20:21 +0100

    Those tests run but don't work

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,18 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main
+where
+  import Lib
+  import Test.Hspec
+  import Data.Typeable
+  import Data.Array.Repa
+  import Codec.Picture
 
-main :: IO ()
-main = hspec $ do
-  describe "Prelude.read" $ do
-    it "can parse integers" $ do
-      read "10" `shouldBe` (10 :: Int)
-
-    it "can parse floating-point numbers" $ do
-      read "2.5" `shouldBe` (2.5 :: Float)
+  main :: IO ()
+  main = hspec $ do
+    describe "Load image" $ do
+      it "reads from file" $ do
+        img <- readImage "lena512.bmp"
+        case img of
+          Left err -> error err
+          Right image ->  do
+                            let arr = fromImageToRepa image
+                            typeOf arr `shouldBe` Array D DIM2 RGBA8
